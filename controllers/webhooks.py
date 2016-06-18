9435565673e65f40f38bd03ef1b60aed8aa305f3
jiajunhuang <gansteed@gmail.com>
2016-06-19 01:42:57 +0800

    github webhooks ping

--- controllers/webhooks.py
+++ controllers/webhooks.py
@@ -7,6 +7,9 @@ from config import Config
 
 
 class WebHooksHandler(tornado.web.RequestHandler):
+    def get(self):  # github webhooks ping
+        self.finish()
+
     def post(self):
         if not os.path.exists(Config().github_webhook_secret_path):
             self.finish()
